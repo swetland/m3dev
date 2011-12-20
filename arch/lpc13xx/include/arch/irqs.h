b2ce90bdc6374019c605384de8e214f2ec044f8a
Brian Swetland <swetland@frotz.net>
2011-12-19 19:09:57 -0800

    lpc13xx: add irq definitions

---
+++ arch/lpc13xx/include/arch/irqs.h
@@ -0,0 +1,77 @@
+/* irqs.h
+ * 
+ * Copyright 2011 Brian Swetland <swetland@frotz.net>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef _IRQ
+	_IRQ(wakeup_0_0)
+	_IRQ(wakeup_0_1)
+	_IRQ(wakeup_0_2)
+	_IRQ(wakeup_0_3)
+	_IRQ(wakeup_0_4)
+	_IRQ(wakeup_0_5)
+	_IRQ(wakeup_0_6)
+	_IRQ(wakeup_0_7)
+	_IRQ(wakeup_0_8)
+	_IRQ(wakeup_0_9)
+	_IRQ(wakeup_0_10)
+	_IRQ(wakeup_0_11)
+	_IRQ(wakeup_1_0)
+	_IRQ(wakeup_1_1)
+	_IRQ(wakeup_1_2)
+	_IRQ(wakeup_1_3)
+	_IRQ(wakeup_1_4)
+	_IRQ(wakeup_1_5)
+	_IRQ(wakeup_1_6)
+	_IRQ(wakeup_1_7)
+	_IRQ(wakeup_1_8)
+	_IRQ(wakeup_1_9)
+	_IRQ(wakeup_1_10)
+	_IRQ(wakeup_1_11)
+	_IRQ(wakeup_2_0)
+	_IRQ(wakeup_2_1)
+	_IRQ(wakeup_2_2)
+	_IRQ(wakeup_2_3)
+	_IRQ(wakeup_2_4)
+	_IRQ(wakeup_2_5)
+	_IRQ(wakeup_2_6)
+	_IRQ(wakeup_2_7)
+	_IRQ(wakeup_2_8)
+	_IRQ(wakeup_2_9)
+	_IRQ(wakeup_2_10)
+	_IRQ(wakeup_2_11)
+	_IRQ(wakeup_3_0)
+	_IRQ(wakeup_3_1)
+	_IRQ(wakeup_3_2)
+	_IRQ(wakeup_3_3)
+	_IRQ(i2c0)
+	_IRQ(ct16b0)
+	_IRQ(ct16b1)
+	_IRQ(ct32b0)
+	_IRQ(ct32b1)
+	_IRQ(ssp0)
+	_IRQ(uart)
+	_IRQ(usb_irq)
+	_IRQ(usb_fiq)
+	_IRQ(adc)
+	_IRQ(wdt)
+	_IRQ(bod)
+	_IRQ(reserved0)
+	_IRQ(gpio3)
+	_IRQ(gpio2)
+	_IRQ(gpio1)
+	_IRQ(gpio0)
+	_IRQ(ssp1)
+#endif
