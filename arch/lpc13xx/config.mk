265b6f3a6137c17d3914d8ab366655476bcb5f2a
Brian Swetland <swetland@frotz.net>
2011-12-19 19:09:26 -0800

    lpc13xx/stm32f1xx: fix typo in config.mk (arch-m3 -> arm-m3)

--- arch/lpc13xx/config.mk
+++ arch/lpc13xx/config.mk
@@ -1,3 +1,3 @@
 ARCH_lpc13xx_CFLAGS := -Iarch/lpc13xx/include
-ARCH_lpc13xx_CFLAGS += -Iarch/arch-cm3/include
+ARCH_lpc13xx_CFLAGS += -Iarch/arm-cm3/include
 ARCH_lpc13xx_CFLAGS += -DCONFIG_STACKTOP=0x10001f00
